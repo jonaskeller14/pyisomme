31c0a4127b845e8c1b8c4018c35a02218c7f1c65
jonaskeller14 <jonaskeller14@googlemail.com>
2024-02-09 12:33:13 +0100

    pyisomme/__init__: Added logging and import report

--- pyisomme/__init__.py
+++ pyisomme/__init__.py
@@ -1,5 +1,12 @@
+import logging
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.getLogger().setLevel(logging.INFO)
+
 from pyisomme.isomme import *
 from pyisomme.channel import *
 from pyisomme.plotting import *
 from pyisomme.unit import *
 from pyisomme.limits import *
+from pyisomme.report import *
+
+from pyisomme.report_euro_ncap_frontal_50kmh import EuroNCAP_Frontal_50kmh
