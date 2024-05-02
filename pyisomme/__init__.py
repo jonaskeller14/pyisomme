8489446d7c8f583a30ae7e4fcad4adc1c0abee82
jonaskeller14 <jonaskeller14@googlemail.com>
2024-05-02 23:37:19 +0200

    pyisomme/correlation: Fix, needed to import Correlation_ISO18571

--- pyisomme/__init__.py
+++ pyisomme/__init__.py
@@ -4,6 +4,7 @@ logging.getLogger().setLevel(logging.INFO)
 
 from pyisomme.isomme import *
 from pyisomme.channel import *
+from pyisomme.correlation import Correlation_ISO18571
 from pyisomme.plotting import *
 from pyisomme.unit import *
 from pyisomme.limits import *
