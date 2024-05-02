dc6f8efc11121c3a32b79109b7f0fbe5daa70a0f
jonaskeller14 <jonaskeller14@googlemail.com>
2024-05-02 22:07:09 +0200

    pyisomme/correlation: Added function to calculate ISO18571 correlation score between two channels

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,9 @@
 import unittest
-import sys
 import os
-import matplotlib.pyplot as plt
-sys.path.append(os.path.join(__file__, "..", ".."))
+import logging
+import pandas as pd
+import numpy as np
+
 import pyisomme
 
 
@@ -42,6 +43,20 @@ class TestIsomme(unittest.TestCase):
         assert isomme.get_test_info("Laboratory test ref. number") == isomme.get_test_info("[XL]abo?atory * ref. number")
         assert isomme.get_test_info("Laboratory test ref. number") == isomme.get_test_info("[XL]abo.atory .* ref. number")
 
+    def test_extend(self):
+        isomme_1 = pyisomme.Isomme(channels=[pyisomme.Channel(code="11HEAD0000H3ACXA", data=None),
+                                             pyisomme.Channel(code="11HEAD0000H3ACYA", data=None),])
+        isomme_2 = pyisomme.Isomme(channels=[pyisomme.Channel(code="11HEAD0000H3ACZA", data=None),])
+        isomme_1.extend(isomme_2)
+        assert len(isomme_1.channels) == 3
+        channel = pyisomme.Channel(code="13HEAD0000H3ACXA", data=None)
+        isomme_1.extend(channel)
+        assert len(isomme_1.channels) == 4
+        channel_list = [pyisomme.Channel(code="13HEAD0000H3ACYA", data=None),
+                        pyisomme.Channel(code="13HEAD0000H3ACZA", data=None)]
+        isomme_1.extend(channel_list)
+        assert len(isomme_1.channels) == 6
+
 
 class TestChannel(unittest.TestCase):
     def test_init(self):
@@ -55,9 +70,9 @@ class TestChannel(unittest.TestCase):
 
 class TestLimits(unittest.TestCase):
     def test_get_limits(self):
-        limits = pyisomme.Limits(limits=[pyisomme.Limit(code_patterns=["11NECKUP????FOX?"], func=lambda x: 500, name="sdfsdf", color="yellow", linestyle="--"),
-                                         pyisomme.Limit(code_patterns=["11NECKUP.*FOX[AB]"], func=lambda x: 500, name="sdfsdf", color="yellow", linestyle="--"),
-                                         pyisomme.Limit(code_patterns=["11NECKUP????FOY?"], func=lambda x: 750 - 7.5*x, name="da", color="red", linestyle="-"), ])
+        limits = pyisomme.Limits(limit_list=[pyisomme.Limit(code_patterns=["11NECKUP????FOX?"], func=lambda x: 500, name="sdfsdf", color="yellow", linestyle="--"),
+                                             pyisomme.Limit(code_patterns=["11NECKUP.*FOX[AB]"], func=lambda x: 500, name="sdfsdf", color="yellow", linestyle="--"),
+                                             pyisomme.Limit(code_patterns=["11NECKUP????FOY?"], func=lambda x: 750 - 7.5*x, name="da", color="red", linestyle="-"), ])
         assert len(limits.get_limits("11NECKUP00H3FOXA")) == 2
 
 
@@ -65,40 +80,41 @@ class TestCalculate(unittest.TestCase):
     pass
 
 
+class TestReport(unittest.TestCase):
+    v1 = pyisomme.Isomme().read(os.path.join(__file__, "..", "..", "data", "nhtsa", "11391"), "1[13]*")
+    v2 = pyisomme.Isomme().read(os.path.join(__file__, "..", "..", "data", "nhtsa", "14084"), "1[13]*")
+
+    def test_EuroNCAP_Frontal_50kmh(self):
+        pyisomme.EuroNCAP_Frontal_50kmh([self.v1, self.v2], "My Report").calculate()
+
+
 class TestPlotting(unittest.TestCase):
     v1 = pyisomme.Isomme().read(os.path.join(__file__, "..", "..", "data", "nhtsa", "11391"), "11NECKUP????FO??", "11TIBI*FO*")
     v2 = pyisomme.Isomme().read(os.path.join(__file__, "..", "..", "data", "nhtsa", "14084"), "11NECKUP????FO??", "11TIBI*FO*")
     v3 = pyisomme.Isomme().read(os.path.join(__file__, "..", "..", "data", "nhtsa", "14065"), "11NECKUP????FO??", "11TIBI*FO*")
 
-    def test_plot_1(self):
-        pyisomme.plot_1(
-            [self.v1, self.v2, self.v3],
-            "11NECKUP????FOX?",
-            limits=pyisomme.Limits(limits=[ pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 560, color="green", name="Good", upper=True),
-                                            pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 700, color="yellow", name="Acceptable", upper=True),
-                                            pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 840, color="orange", name="Marginal", upper=True),
-                                            pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 840, color="red", name="Poor", lower=True),]))
-        plt.savefig(os.path.join(__file__, "..", "out", "TestPlotting_test_plot_1_1.jpg"))
 
-    def test_plot_1_xyzr(self):
-        pyisomme.plot_1_xyzr(
-            [self.v1, self.v2, self.v3],
-            "11NECKUP????FO??",
-            limits=pyisomme.Limits(limits=[ pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 560, color="green", name="Good", upper=True),
-                                            pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 700, color="yellow", name="Acceptable", upper=True),
-                                            pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 840, color="orange", name="Marginal", upper=True),
-                                            pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 840, color="red", name="Poor", lower=True),]))
-        plt.savefig(os.path.join(__file__, "..", "out", "TestPlotting_test_plot_1_xyzr_1.jpg"))
+class TestCorrelation(unittest.TestCase):
+    def test_correlation(self):
+        reference_channel = pyisomme.create_sample(t_range=(0, 0.1, 1000), y_range=(0, 10))
+        comparison_channel = pyisomme.create_sample(t_range=(0, 0.11, 1000), y_range=(0, 11))
+        correlation = pyisomme.Correlation_ISO18571(reference_channel, comparison_channel)
+        print(correlation.overall_rating())
 
-    def test_plot_4x1_xyzr(self):
-        pyisomme.plot_4x1_xyzr(
-            [self.v1, self.v2, self.v3],
-            "11NECKUP????FO??",
-            limits=pyisomme.Limits(limits=[ pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 560, color="green", name="Good", upper=True),
-                                            pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 700, color="yellow", name="Acceptable", upper=True),
-                                            pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 840, color="orange", name="Marginal", upper=True),
-                                            pyisomme.Limit(["11NECKUP????FOX?"], func=lambda x: 840, color="red", name="Poor", lower=True),]))
-        plt.savefig(os.path.join(__file__, "..", "out", "TestPlotting_test_plot_4x1_xyzr_1.jpg"))
+    def test_correlation2(self):
+        time = np.arange(0, 0.150, 0.0001)
+        reference = np.sin(time * 20)
+        comparison = np.sin(time * 20) * 1.3 + 0.00
+
+        reference_channel = pyisomme.Channel(code="????????????????",
+                                    data=pd.DataFrame(reference, index=time))
+        comparison_channel = pyisomme.Channel(code="????????????????",
+                                     data=pd.DataFrame(comparison, index=time))
+
+        correlation = pyisomme.Correlation_ISO18571(reference_channel, comparison_channel)
+        overall_rating = correlation.overall_rating()
+        assert np.abs(overall_rating - 0.713) < 1e-6
+        logging.info(correlation.overall_rating())
 
 
 if __name__ == '__main__':
