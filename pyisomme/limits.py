pyisomme/report/euro_ncap: Fixed Chest VC Criterion, take nan values into account for limit value auto calculation

pyisomme/limits.py
@@ -143,10 +143,10 @@ class Limits:
         return limit_values
 
     def get_limit_max_value(self, channel: Channel, interpolate=True) -> float:
-        return np.max(self.get_limit_values(channel, interpolate))
+        return np.nanmax(self.get_limit_values(channel, interpolate))
 
     def get_limit_min_value(self, channel: Channel, interpolate=True) -> float:
-        return np.min(self.get_limit_values(channel, interpolate))
+        return np.nanmin(self.get_limit_values(channel, interpolate))
 
     def get_limit_min_color(self, channel: Channel):
         limit_values = self.get_limit_values(channel, interpolate=False)

pyisomme/report/euro_ncap/frontal_50kmh.py
@@ -433,18 +433,16 @@ class EuroNCAP_Frontal_50kmh(Report):
                         self.p = p
 
                         self.extend_limit_list([
-                            Limit_G([f"?{self.p}VCCR0000??VEX?"], func=lambda x: 0.500, y_unit="m/s", upper=True),
-                            Limit_A([f"?{self.p}VCCR0000??VEX?"], func=lambda x: 0.500, y_unit="m/s", lower=True),
-                            Limit_M([f"?{self.p}VCCR0000??VEX?"], func=lambda x: 0.667, y_unit="m/s", lower=True),
-                            Limit_W([f"?{self.p}VCCR0000??VEX?"], func=lambda x: 0.833, y_unit="m/s", lower=True),
-                            Limit_P([f"?{self.p}VCCR0000??VEX?"], func=lambda x: 1.000, y_unit="m/s"),
-                            Limit_C([f"?{self.p}VCCR0000??VEX?"], func=lambda x: 1.000, y_unit="m/s", lower=True),
+                            Limit_G([f"?{self.p}VCCR000[03]??VEX?"], func=lambda x: 0.500, y_unit="m/s", upper=True),
+                            Limit_A([f"?{self.p}VCCR000[03]??VEX?"], func=lambda x: 0.500, y_unit="m/s", lower=True),
+                            Limit_M([f"?{self.p}VCCR000[03]??VEX?"], func=lambda x: 0.667, y_unit="m/s", lower=True),
+                            Limit_W([f"?{self.p}VCCR000[03]??VEX?"], func=lambda x: 0.833, y_unit="m/s", lower=True),
+                            Limit_P([f"?{self.p}VCCR000[03]??VEX?"], func=lambda x: 1.000, y_unit="m/s"),
+                            Limit_C([f"?{self.p}VCCR000[03]??VEX?"], func=lambda x: 1.000, y_unit="m/s", lower=True),
                         ])
 
                     def calculation(self):
-                        chest_deflection = (self.isomme.get_channel(f"?{self.p}CHST0003??DSXC", f"?{self.p}CHST0000??DSXC"))
-
-                        self.channel = calculate_chest_vc(chest_deflection).convert_unit("m/s")
+                        self.channel = self.isomme.get_channel(f"?{self.p}VCCR0003??VEXC", f"?{self.p}VCCR0000??VEXC").convert_unit("m/s")
                         self.value = np.min(self.channel.get_data())
                         self.rating = self.limits.get_limit_min_value(self.channel, interpolate=True)
 

pyisomme/report/euro_ncap/frontal_mpdb.py
@@ -728,7 +728,7 @@ class EuroNCAP_Frontal_MPDB(Report):
 
                         def calculation(self):
                             self.channel = self.isomme.get_channel(f"?{self.p}NECKUP00??FOXB").convert_unit("kN")
-                            self.value = self.channel.get_data(unit="kN")[np.argmax(np.abs(self.channel.get_data("kN")))]
+                            self.value = self.channel.get_data(unit="kN")[np.argmax(np.abs(self.channel.get_data(unit="kN")))]
                             self.rating = self.limits.get_limit_min_value(self.channel)
 
                     class Criterion_Fz_Tension(Criterion):