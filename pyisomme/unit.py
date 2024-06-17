2e597c599d12353187f67f4a6315660deb3ca1bc
jonaskeller14 <jonaskeller14@googlemail.com>
2024-06-17 23:08:11 +0200

    pyisomme/unit: Bugfix, Convert Celsius to deg

--- pyisomme/unit.py
+++ pyisomme/unit.py
@@ -1,3 +1,4 @@
+import re
 import astropy.units as u
 from astropy.constants import g0
 
@@ -10,5 +11,5 @@ g0 = g0
 class Unit:
     def __new__(cls, unit):
         if isinstance(unit, str):
-            unit = unit.replace("°", "deg")
+            unit = re.sub(r"(°)[^C]?", "deg", unit)
         return u.Unit(unit)
