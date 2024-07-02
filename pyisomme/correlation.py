74bb7a7befc97a28c76c1621467f1e1661d5b7cb
jonaskeller14 <jonaskeller14@googlemail.com>
2024-07-02 18:18:17 +0200

    pyisomme/correlation: Adjust Channel data by interpolation to met 10kHz criterion

--- pyisomme/correlation.py
+++ pyisomme/correlation.py
@@ -14,7 +14,10 @@ class Correlation_ISO18571:
 
         time = time_intersect(reference_channel, comparison_channel)
         if not np.all(np.abs(np.diff(time) - 0.0001) < 10e-6):
-            logger.warning("Time resolution of 10 kHz required")
+            logger.warning("Time resolution of 10 kHz required. Channel Data will be adjusted by interpolation.")
+            t_start = time[0]
+            t_end = time[-1]
+            time = np.arange(t_start, t_end, 0.0001)
 
         reference_curve = np.vstack((time, reference_channel.get_data(t=time))).T
         comparison_curve = np.vstack((time, comparison_channel.get_data(t=time, unit=reference_channel.unit))).T
