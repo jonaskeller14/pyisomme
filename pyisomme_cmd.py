508ff210550a5d495b0ad1e354cb62f451ae81cf
jonaskeller14 <jonaskeller14@googlemail.com>
2024-07-09 20:40:28 +0200

    pyisomme_cmd: Added 'list' command and arguments to filter/set channels codes

--- pyisomme_cmd.py
+++ pyisomme_cmd.py
@@ -24,13 +24,37 @@ def main():
                         datefmt='%m/%d/%Y %I:%M:%S',
                         level=logging.INFO if options.verbose else logging.WARNING)
 
+    if options.command == 'list':
+        for isomme in [pyisomme.Isomme().read(input_path) for input_path in options.input_paths]:
+            print("\n")
+            print(isomme.test_number)
+
+            if len(options.codes) == 0:
+                channels = isomme.channels
+            else:
+                channels = isomme.get_channels(*options.codes, filter=False, calculate=False, integrate=False, differentiate=False)
+
+            for channel in channels:
+                print(channel.code)
+
     if options.command == "merge":
-        merged_isomme = pyisomme.Isomme().read(options.input_paths[0])
+        merged_isomme = pyisomme.Isomme().read(options.input_paths[0], *options.codes)
         for other_input_path in options.input_paths[1:]:
-            merged_isomme.extend(pyisomme.Isomme().read(other_input_path))
+            merged_isomme.extend(pyisomme.Isomme().read(other_input_path, *options.codes))
 
         if options.delete_duplicates or options.delete_filter_duplicates:
             merged_isomme.delete_duplicates(filter_class_duplicates=options.delete_filter_duplicates)
+
+        for channel in merged_isomme.channels:
+            channel.set_code(test_object=options.test_object,
+                             position=options.position,
+                             main_location=options.main_location,
+                             fine_location_1=options.fine_location_1,
+                             fine_location_2=options.fine_location_2,
+                             fine_location_3=options.fine_location_3,
+                             physical_dimension=options.physical_dimension,
+                             direction=options.direction,
+                             filter_class=options.filter_class)
 
         merged_isomme.write(options.output_path)
 
@@ -49,13 +73,26 @@ if __name__ == "__main__":
 
     command_parsers = parser.add_subparsers(dest="command")
 
+    list_parser = command_parsers.add_parser("list")
+    list_parser.add_argument(nargs="+",
+                             dest="input_paths",
+                             help="ISO-MME Path (.mme/folder/.zip/.tar/.tar.gz/...)")
+    list_parser.add_argument("-c", "--codes",
+                             nargs="*",
+                             dest="codes",
+                             default=[],
+                             help="Channel Code Patterns to filter ISO-MMEs")
+
     merge_parser = command_parsers.add_parser("merge", help="Merge ISO-MMEs")
-    merge_parser.add_argument(nargs=1,
-                              dest="output_path",
+    merge_parser.add_argument(dest="output_path",
                               help="ISO-MME Path (.mme/folder/.zip/.tar/.tar.gz/...)")
     merge_parser.add_argument(nargs="+",
                               dest="input_paths",
                               help="ISO-MME Path (.mme/folder/.zip/.tar/.tar.gz/...)")
+    merge_parser.add_argument("-c", "--codes",
+                              nargs="*",
+                              dest="codes",
+                              help="Channel Code Patterns to filter ISO-MMEs")
     merge_parser.add_argument("--delete-duplicates",
                               action="store_true",
                               dest="delete_duplicates",
@@ -65,6 +102,15 @@ if __name__ == "__main__":
                               dest="delete_filter_duplicates",
                               help="Delete filtered channels if channel with less filtering exists. "
                                    "Includes '--delete-duplicates' option")
+    merge_parser.add_argument("--set-test-object", dest="test_object")
+    merge_parser.add_argument("--set-position", dest="position")
+    merge_parser.add_argument("--set-main-location", dest="main_location")
+    merge_parser.add_argument("--set-fine-location-1", dest="fine_location_1")
+    merge_parser.add_argument("--set-fine-location-2", dest="fine_location_2")
+    merge_parser.add_argument("--set-fine-location-3", dest="fine_location_3")
+    merge_parser.add_argument("--set-physical-dimension", dest="physical_dimension")
+    merge_parser.add_argument("--set-direction", dest="direction")
+    merge_parser.add_argument("--set-filter-class", dest="filter_class")
 
     report_parser = command_parsers.add_parser("report", help="Create a Report")
     report_parser.add_argument(dest="report_name",
